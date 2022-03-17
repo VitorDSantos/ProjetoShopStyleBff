879aef7e21fce852e3660ca7635b835614525a0a
VitorDSantos <vv19991019@gmail.com>
2022-03-17 09:28:38 -0300

    adicinando arquivos bff-shop

---
+++ src/main/java/msbffshop/MsBffShopApplication.java
@@ -0,0 +1,13 @@
+package msbffshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBffShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBffShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msbffshop/MsBffShopApplicationTests.java
@@ -0,0 +1,13 @@
+package msbffshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBffShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
